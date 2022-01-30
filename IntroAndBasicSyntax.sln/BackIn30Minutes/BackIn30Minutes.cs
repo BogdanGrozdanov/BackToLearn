ad6227b9bf6011b5e4de730ab50b3d0b22185cf3
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-01-30 13:04:02 +0000

    Create BackIn30Minutes.cs

---
+++ IntroAndBasicSyntax.sln/BackIn30Minutes/BackIn30Minutes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    internal class BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
