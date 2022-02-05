2b451db296694f663a97083db7018660b9de83f3
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-02-05 11:04:13 +0000

    Create Multiplication Table.cs

---
+++ IntroAndBasicSyntax.sln/MultiplicationTable/Multiplication Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger =int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int product=theInteger*i;
+                Console.WriteLine($"{theInteger} X {i} = {product}");
+            }
+            
+        }
+    }
+}
