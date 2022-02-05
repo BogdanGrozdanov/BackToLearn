8c41701c1730db6d191966d973abbf7d43cd7cf7
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-02-05 11:53:35 +0000

    Create EvenNumber.cs

---
+++ IntroAndBasicSyntax.sln/EvenNumber/EvenNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    internal class EvenNumber
+    {
+        static void Main(string[] args)
+        {
+            int evenNum = int.Parse(Console.ReadLine());
+
+            while (evenNum % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                evenNum=int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {evenNum}");
+           
+        }
+    }
+}
