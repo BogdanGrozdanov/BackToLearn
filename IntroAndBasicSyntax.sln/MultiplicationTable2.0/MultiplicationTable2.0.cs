8558d0da362419c4b25d7957e9992205a81979ce
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-02-05 11:04:17 +0000

    Create MultiplicationTable2.0.cs

---
+++ IntroAndBasicSyntax.sln/MultiplicationTable2.0/MultiplicationTable2.0.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            if (times > 10)
+            {
+                Console.WriteLine($"{theInteger} X {times} = {theInteger * times}");
+            }
+            else
+            {
+                for (int i = times; i <= 10; i++)
+                {
+
+                    Console.WriteLine($"{theInteger} X {i} = {theInteger * i}");
+                }
+            }
+
+        }
+    }
+}
