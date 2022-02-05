1ca9abe92ef1666ccc518bdbc225bea4d7afc08c
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-02-05 10:19:50 +0000

    Create ForeignLanguages.cs

---
+++ IntroAndBasicSyntax.sln/ForeignLanguages/ForeignLanguages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    internal class ForeignLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine().ToLower();
+
+            switch (country)
+            {
+                case "england":
+                case "usa":
+                    Console.WriteLine("English");
+                    break;
+                case "spain":
+                case "argentina":
+                case "mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
