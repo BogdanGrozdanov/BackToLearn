f4586ad0d1d165d93c2354f93b6a798e0626e32d
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-02-06 12:58:34 +0000

    Create PrintAndSum.cs

---
+++ ExerciseIntroAndBasicSyntax/PrintAndSum/PrintAndSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    internal class PrintAndSum
+    {
+        static void Main(string[] args)
+        {
+            int startNun = int.Parse(Console.ReadLine());
+            int endNun = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = startNun; i <= endNun; i++)
+            {
+                Console.Write($" {i}");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
