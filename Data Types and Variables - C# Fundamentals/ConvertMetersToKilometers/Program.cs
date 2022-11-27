a48799c27fbd5e78d9986b9f4969950e06d695e6
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-11-27 19:26:08 +0200

    Delete Program.cs

--- Data Types and Variables - C# Fundamentals/ConvertMetersToKilometers/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    public static void Main(string[] args)
-    {
-        int input = int.Parse(Console.ReadLine());
-        double output = input / 1000.00;
-        Console.WriteLine($"{output:F2}");
-    }
-}
