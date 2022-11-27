b88f89b83cc15ddf094f56537bd76051f0b742ac
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-11-27 21:37:23 +0200

    Create Exact Sum of Real Numbers.cs

---
+++ ExactSumOfRealNumbers/Exact Sum of Real Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace ExactSumOFRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                decimal input = decimal.Parse(Console.ReadLine());
+                sum += input;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
