50c8f19b8500358ab3479117e3bc5582ac04f010
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-11-27 21:21:17 +0200

    Create PoundsToDollars.cs

---
+++ Data Types and Variables - C# Fundamentals/PoundsToDollars/PoundsToDollars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PoundsToDollars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal dolarPerPound = 1.31M;
+            decimal output = input * dolarPerPound;
+
+            Console.WriteLine($"{output:F3}");
+        }
+    }
+}
