d508c7c952e3cebe96b3a7a3e93952433f138b9d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-11-27 19:26:04 +0200

    Create ConvertMetersToKilometers.cs

---
+++ Data Types and Variables - C# Fundamentals/ConvertMetersToKilometers/ConvertMetersToKilometers.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        double output = input / 1000.00;
+        Console.WriteLine($"{output:F2}");
+    }
+}
