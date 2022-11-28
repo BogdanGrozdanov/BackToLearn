f26e6a7bd4dc085d92ad7c7cfd192ef7dad5bcc0
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2022-11-28 20:17:18 +0200

    Create CenturiesToMinutes.cs

---
+++ Data Types and Variables - C# Fundamentals/CenturiesToMinutes/CenturiesToMinutes.cs
@@ -0,0 +1,18 @@
+﻿namespace CenturiesToMinutes
+{
+    internal class CenturiesToMinutes
+    {
+        static void Main(string[] args)
+        {
+            double centuries = int.Parse(Console.ReadLine());
+
+            double years = centuries * 100;
+            double days = Math.Round(years * 365.2422);
+            double hours = days * 24;
+            double minutes = hours * 60;
+            string output = $"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes";
+
+            Console.WriteLine(output);  
+        }
+    }
+}
