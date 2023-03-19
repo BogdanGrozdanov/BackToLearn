2d8b7b177f9669c8b118955e59d9ad76ba00162b
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-19 17:46:02 +0200

    Negative or Positive number
    Print sign numbers is positive or negative

---
+++ Methods/MethodsSample/PrintSign.cs
@@ -0,0 +1,24 @@
+﻿namespace MethodsSample
+{
+    internal class PrintSign
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintNumberSign(number);
+        }
+        static void PrintNumberSign(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive");
+            }
+            else { Console.WriteLine($"The number {number} is zero"); }
+
+        }
+    }
+}
